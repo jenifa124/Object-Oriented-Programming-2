c0db4a12deceb842de318251129cceb86c026e93
Most.Jenifa Akter <114852184+jenifa124@users.noreply.github.com>
2026-04-22 14:35:46 +0600

    login page
    here, C# visual studio net frame work i create a login page

---
+++ FinalProject/FinalProject/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbusername = new System.Windows.Forms.Label();
+            this.lbpassword = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbusername
+            // 
+            this.lbusername.AutoSize = true;
+            this.lbusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbusername.Location = new System.Drawing.Point(53, 72);
+            this.lbusername.Name = "lbusername";
+            this.lbusername.Size = new System.Drawing.Size(103, 20);
+            this.lbusername.TabIndex = 0;
+            this.lbusername.Text = "UserName:";
+            // 
+            // lbpassword
+            // 
+            this.lbpassword.AutoSize = true;
+            this.lbpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbpassword.Location = new System.Drawing.Point(53, 135);
+            this.lbpassword.Name = "lbpassword";
+            this.lbpassword.Size = new System.Drawing.Size(97, 20);
+            this.lbpassword.TabIndex = 1;
+            this.lbpassword.Text = "Password:";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.Color.Lavender;
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnlogin.Location = new System.Drawing.Point(222, 254);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(97, 41);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "LOGIN";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(162, 72);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(233, 22);
+            this.txtusername.TabIndex = 3;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(162, 135);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(233, 22);
+            this.txtpassword.TabIndex = 4;
+            this.txtpassword.UseSystemPasswordChar = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(579, 435);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.lbpassword);
+            this.Controls.Add(this.lbusername);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOGIN VIWE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbusername;
+        private System.Windows.Forms.Label lbpassword;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+    }
+}
+
---
+++ FinalProject/FinalProject/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=DESKTOP-9SQ0N8R\\SQLEXPRESS;Initial Catalog=MyDatabase2;Integrated Security=True";
+
+            string UserName = txtusername.Text.Trim();
+            string Password = txtpassword.Text.Trim();
+
+            string query = "Select * from Users where UserName='"
+                + UserName + "' and Password='" + Password + "'";
+            SqlCommand command = new SqlCommand(query, con);
+
+            con.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                     Users user = new Users()
+                    {
+                        Id = reader.GetInt32(0),
+                        Username = reader.GetString(1),
+                        Password = reader.GetString(2),
+                        Userstype = reader.GetInt32(3)
+
+                    };
+                        con.Close();
+                        
+                        MessageBox.Show("login  successfull");
+                        break;
+                    }
+                }
+                //this.Hide();
+                //Form1 form1 = new Form1()
+                else
+                {
+                    MessageBox.Show("Invalid user");
+                }
+            }
+        }
+        //bool isVisible = false;
+        //private void Pnbpassword_Click(object sender, EventArgs e)
+        //{
+        //    if (isVisible == false)
+        //    {
+        //        txtpassword.UseSystemPasswordChar = false; // show password
+        //        isVisible = true;
+        //    }
+        //    else
+        //    {
+        //        txtpassword.UseSystemPasswordChar = true; // hide password
+        //        isVisible = false;
+        //   }
+        //}
+    }
+}
+
+
